de3269ca2c071cf6d7e1a1a2a8ce5feb7b820c45
JonasSantos1430 <jonascj2010@hotmail.com>
2021-08-17 17:24:58 -0300

    Alteração-Form-ProgressBar

--- Controle-Acessos-Umuarama/Tela_Load.Designer.cs
+++ Controle-Acessos-Umuarama/Tela_Load.Designer.cs
@@ -73,7 +73,7 @@ namespace Controle_Acessos_Umuarama
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Impact", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label1.Location = new System.Drawing.Point(131, 358);
+            this.label1.Location = new System.Drawing.Point(131, 368);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(207, 26);
             this.label1.TabIndex = 2;
